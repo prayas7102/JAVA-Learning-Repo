778d409acab357b86ebc32f86f70dc3f4c2b5569
prayas7102 <prayas.kumar@coriolis.co.in>
2023-06-15 06:05:08 +0530

    TwoSum

--- Question-9-b.java
+++ Question-9-b.java
@@ -1,23 +1,35 @@
-import java.util.*;
-public class TwoSum
-{
-    public static void findTwoSum(int[] arr, int targetSum) {
-        HashSet<Integer> targetSet = new HashSet<>();
-        for (int i = 0; i < arr.length; i++) {
-            int target = targetSum - arr[i];
-            if (targetSet.contains(target)) {
-                System.out.println(arr[i] + ", " + target);
-                return;
-            }
-            targetSet.add(arr[i]);
+// Write a program without using any libraries in java that, 
+// given an array A[] of n numbers and another number x, 
+// determines whether or not there exist two elements in 
+// S whose sum is exactly x. 
+// # Input: arr[] = {0, -1, 2, -3, 1},  sum=-2
+// # output: -3, 1
+
+public class TwoSum {
+
+  public static void main(String[] args) {
+    int[] arr = { 0, -1, 2, -3, 1 };
+    int targetSum = -2;
+    // to check if there's required element which sum up to targetsum
+    boolean found = false;
+    // traversing array in two loops and comparing all elements with each other
+    for (int i = 0; i < arr.length; i++) {
+      int num1 = arr[i];
+      for (int j = i + 1; j < arr.length; j++) {
+        int num2 = arr[j];
+        if (num1 + num2 == targetSum) {
+          System.out.println(num1 + ", " + num2);
+          found = true;
+          break;
         }
-        System.out.println("No two elements sum ");
+      }
+      if (found) {
+        break;
+      }
     }
 
-    public static void main(String[] args) {
-        int[] arr1 = {0, -1, 2, -3, 1};
-        int sum1 = -2;
-        System.out.print("Output: ");
-        findTwoSum(arr1, sum1);
+    if (!found) {
+      System.out.println("No two elements sum to " + targetSum);
     }
+  }
 }
