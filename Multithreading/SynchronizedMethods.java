1b3098542482e28a8f7c784b9f986a47e679daa9
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-07-04 13:51:42 +0530

    FOLDER CHANGING

--- Multithreading/SynchronizedMethods.java
+++ Multithreading/SynchronizedMethods.java
@@ -5,21 +5,43 @@ import java.util.List;
 import java.util.Random;
 
 class SynchronizedMethods {
-    public static void main(String args[]) {
-        System.out.println("Starting ...");
-        long start = System.currentTimeMillis();
-        process();
-        long end = System.currentTimeMillis();
-        System.out.println("Time taken: " + (end - start));
-        System.out.println("List1: " + list1.size());
-        System.out.println("List2: " + list2.size());
-    }
 
     private static Random random = new Random();
     private static Object lock1 = new Object();
     private static Object lock2 = new Object();
     private static List<Integer> list1 = new ArrayList<Integer>();
     private static List<Integer> list2 = new ArrayList<Integer>();
+    public static void main(String args[]) {
+        System.out.println("Starting ...");
+        long start = System.currentTimeMillis();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                process();
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                process();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken: " + (end - start));
+        System.out.println("List1: " + list1.size());
+        System.out.println("List2: " + list2.size());
+    }
 
     public static void process() {
         for (int i = 0; i < 1000; i++) {
@@ -28,7 +50,7 @@ class SynchronizedMethods {
         }
     }
 
-    public static void stageOne() {
+    public synchronized static void stageOne() {
         try {
             Thread.sleep(1);
         } catch (InterruptedException e) {
@@ -37,8 +59,7 @@ class SynchronizedMethods {
         list1.add(random.nextInt(100));
     }
 
-    public static void stageTwo() {
-
+    public synchronized static void stageTwo() {
         try {
             Thread.sleep(1);
         } catch (InterruptedException e) {
@@ -47,3 +68,23 @@ class SynchronizedMethods {
         list2.add(random.nextInt(100));
     }
 }
+//        In general, using threads in a Java program can potentially
+//        improve performance by allowing concurrent execution of
+//        tasks. By utilizing multiple threads, a program can take
+//        advantage of parallelism and execute multiple processes
+//        simultaneously, potentially reducing overall execution time.
+
+//    Without using threads:
+//        Time taken:4103
+//        List1:1000
+//        List2:1000
+
+//    With using threads:
+//        Time taken: 3972
+//        List1: 1995
+//        List2: 1995
+
+//    With using synchronized on stage1 & stage2 threads:
+//        Time taken: 7981
+//        List1: 2000
+//        List2: 2000
