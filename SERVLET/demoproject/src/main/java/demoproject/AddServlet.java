4f30df4bc2561d4e854880145ba33476407f9568
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-07-07 15:28:13 +0530

    InterServlet communication

--- SERVLET/demoproject/src/main/java/demoproject/AddServlet.java
+++ SERVLET/demoproject/src/main/java/demoproject/AddServlet.java
@@ -15,11 +15,22 @@ public class AddServlet extends HttpServlet {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i + j;
+
+//		1. similar to res.send in nodejs	
 //		System.out.print(k);
 //		PrintWriter out = res.getWriter();
 //		out.println("result is " + k);
-		req.setAttribute("k", k);
-		RequestDispatcher rd = req.getRequestDispatcher("/sq");
-		rd.forward(req, res);
+
+//      2. InterServlet communication (session saving)
+
+//		2.a. session saving by passing k value to other servlet
+//		req.setAttribute("k", k);
+
+//		2.b. calling SquareServlet from AddServlet
+//		RequestDispatcher rd = req.getRequestDispatcher("/sq");
+//		rd.forward(req, res);
+
+//		2.c. passing k as parameter
+		res.sendRedirect("sq?k=" + k);
 	}
 }
--- SERVLET/demoproject/src/main/java/demoproject/SquareServlet.java
+++ SERVLET/demoproject/src/main/java/demoproject/SquareServlet.java
@@ -10,9 +10,19 @@ import jakarta.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public class SquareServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		int k = (int) req.getAttribute("k");
-		k = k*k;
+
+//      InterServlet communication (session saving)	
+		
+// 		2.a. session saving by passing k value to other servlet
+// 		get k value from req (attribute)
+//		int k = (int) req.getAttribute("k");
+
+//		2.c. passing k as parameter
+// 		get k value from req (parameter)
+		int k = Integer.parseInt(req.getParameter("k"));
+		k = k * k;
+		
 		PrintWriter out = res.getWriter();
-		out.println("SquareServlet" + k);
+		out.println("SquareServlet:  " + k);
 	}
 }
