620efa93edb8368b6075c1a83770cdd635c46f6f
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-07-07 14:18:36 +0530

    SquareServlet

--- SERVLET/demoproject/src/main/java/demoproject/AddServlet.java
+++ SERVLET/demoproject/src/main/java/demoproject/AddServlet.java
@@ -3,18 +3,23 @@ package demoproject;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class AddServlet extends HttpServlet {
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i + j;
-		System.out.print(k);
-		PrintWriter out = res.getWriter();
-		out.println("result is " + k);
+//		System.out.print(k);
+//		PrintWriter out = res.getWriter();
+//		out.println("result is " + k);
+		req.setAttribute("k", k);
+		RequestDispatcher rd = req.getRequestDispatcher("/sq");
+		rd.forward(req, res);
 	}
 }
