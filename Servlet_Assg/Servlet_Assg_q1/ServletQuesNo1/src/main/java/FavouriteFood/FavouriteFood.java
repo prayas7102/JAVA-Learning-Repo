26a15e99321ac616110fd604fd8f0a06257bc210
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-07-13 17:45:21 +0530

    simple projects on servlet

---
+++ Servlet_Assg/Servlet_Assg_q1/ServletQuesNo1/src/main/java/FavouriteFood/FavouriteFood.java
@@ -0,0 +1,17 @@
+package FavouriteFood;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/FavouriteFood")
+public class FavouriteFood extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+
+	}
+}
